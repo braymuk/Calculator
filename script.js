1ad4bf2af2cceccd6d7a9f74c132138d7799d637
braymuk <braydeng9@gmail.com>
2021-07-02 13:07:08 -0700

    Basic math functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+let a  = 4;
+let b = 2;
+
+console.log(add(a,b)); // 6
+console.log(subtract(a,b)); // 2
+console.log(multiply(a,b)); // 8
+console.log(divide(a,b)); // 2
