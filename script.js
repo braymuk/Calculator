c0f8ebc6ab7ee7e363e8493cbab76b927f885839
braymuk <braydeng9@gmail.com>
2021-07-02 17:11:06 -0700

    Basic calculator functionality

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(a,b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a,b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a,b) {
-    return a * b;
+    return parseInt(a) * parseInt(b);
 }
 
 function divide(a,b) {
-    return a/b;
+    return parseInt(a)/parseInt(b);
 }
 
 function operate(operator, a, b) {
@@ -34,29 +34,69 @@ function operate(operator, a, b) {
     }
 }
 
-let a  = 4;
-let b = 2;
+let inputNumA;
+let inputNumB;
+let currentInput;
+let operatorInput = '';
+let operatorActive = false;
+
+const history = document.querySelector('#history');
+const solution = document.querySelector('#solution');
 
 
-
-const buttons = document.querySelectorAll('.button');
-
-
-buttons.forEach((e) => {
-    console.log(e);
+const equal = document.querySelector('#equal');
+equal.addEventListener('click', ()=> {
+    inputNumB = solution.textContent;
+    console.log("Operator input:" + operatorInput);
+    console.log("NumA: " + inputNumA);
+    console.log("NumB: " + inputNumB);
+    solution.textContent = operate(operatorInput, inputNumA, inputNumB);
+  
 });
 
-// [].forEach.call(buttons, (div, row, column)=> {
-//     div.style.gridRow = row;
-//     row++;
-//     div.style.gridColumn = column;
-//     column++;
-//     if(row > 4) {
-//         row = 0;
-//     }
-//     if(column > 5) {
-//         column = 0;
-//     }
 
-// });
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator)=> {
+    operator.addEventListener('click', ()=> {
+        if(inputNumA !=undefined && inputNumB != undefined) {
+            solution.textContent = operate(operatorInput, inputNumA, inputNumB);
+        }
+        operatorInput = operator.textContent;
+        if(inputNumA != undefined) {
+            inputNumB = solution.textContent;
+            operatorActive = false;
+            //operate(operatorInput, inputNumA, inputNumB);
+        } else {
+            inputNumA = solution.textContent;
+            operatorActive = true;
+        }
 
+        
+    });
+});
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number)=> {
+    number.addEventListener('click', ()=> {
+        //Reset Number if Operator active
+        if(operatorActive) {
+            solution.textContent = "";
+            operatorActive = false;
+        }
+        //Inputting Number
+        currentInput = solution.textContent + number.textContent;
+        solution.textContent = currentInput;
+
+        //Other
+    });
+});
+
+const ac = document.querySelectorAll('.clear');
+ac.forEach((c)=> {
+    c.addEventListener('click', ()=> {
+        solution.textContent = "";
+        inputNumA = undefined;     
+        inputNumB = undefined;     
+        operatorInput = undefined;
+    });
+});
